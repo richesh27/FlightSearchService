74f603d381a8444b3664ac910b092bb546c95406
richesh27 <kunwarrichesh@gmail.com>
2023-05-15 22:47:47 +0530

    added seeders,migrations,config,readme files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express =  require("express");
+const bodyParser = require('body-parser');
 const {PORT} = require("./config/serverconfig");
 
 const setupAndStartServer  = async() =>{
 
-const app = express();
+    //create an express object
+    const app = express();
 
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended : true}));
 
     app.listen(PORT, ()=>{
         console.log(`Server running at port ${PORT}`);
